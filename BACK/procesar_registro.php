f7ad00bbeda284fe5626cc9e79512633570fe71e
JIRT2007 <juanignaciorios2007@gmail.com>
2025-06-09 19:12:32 +0000

    Desarrollo inicial del registro

--- BACK/procesar_registro.php
+++ BACK/procesar_registro.php
@@ -1,21 +1,24 @@
-<?php
+<?php 
+// Conexión a la base de datos
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'registro_jugadores';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {$nombre = trim($_POST["nombre"]);
+echo "Conexión exitosa";
+  die();
 
-    if (!empty($nombre)) {
- 
-        header("Location: /FRONT/version.html");
-        exit();
-        
-    } else {
-        
-        header("Location: ../FRONT/Registro.html"); 
-        exit();
-    }
-} else {
-    
-    header("Location: ../FRONT/Registro.html");
-    exit();
+$conn = mysqli_connect($hostname, $username, $password, $database);
+if (!$conn) {
+    die('Fallo la conexión: ' . mysqli_connect_error());
 }
+  $nombre = $_POST["Nombre"];
 
+$query = "INSERT INTO registro_jugadores.jugadores (id, nombre, fecha_registro) 
+          VALUES ('$id', '$nombre', '$fecha_registro')";
+$result = mysqli_query($conn, $query);
+
+$_SESSION["ID"] = $id;
+$_SESSION["Nombre"] = $nombre;
+$_SESSION["Fecha"] = $fecha_registro;;
 ?>
