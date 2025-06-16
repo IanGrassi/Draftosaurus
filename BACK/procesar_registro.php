318a4bb967e3087b1042d7f8b7a87f57101a94b3
JIRT2007 <juanignaciorios2007@gmail.com>
2025-06-16 13:47:20 -0300

    Desarrolllo avanzado del PHP del registro

--- BACK/procesar_registro.php
+++ BACK/procesar_registro.php
@@ -5,20 +5,28 @@ $username = 'root';
 $password = '';
 $database = 'registro_jugadores';
 
-echo "Conexión exitosa";
-  die();
-
 $conn = mysqli_connect($hostname, $username, $password, $database);
 if (!$conn) {
     die('Fallo la conexión: ' . mysqli_connect_error());
 }
-  $nombre = $_POST["Nombre"];
+  $nombre = $_POST["nombre"];
 
-$query = "INSERT INTO registro_jugadores.jugadores (id, nombre, fecha_registro) 
-          VALUES ('$id', '$nombre', '$fecha_registro')";
+$query = "INSERT INTO registro_jugadores.jugadores (nombre) 
+          VALUES ('$nombre')";
 $result = mysqli_query($conn, $query);
 
+$id = mysqli_insert_id($conn);
 $_SESSION["ID"] = $id;
-$_SESSION["Nombre"] = $nombre;
-$_SESSION["Fecha"] = $fecha_registro;;
+$_SESSION["Nombre"] = $nombre;;
+
+ header("Location: ../FRONT/version.html");
+        exit();
 ?>
+
+<!--
+CREATE DATABASE registro_jugadores;
+
+CREATE TABLE jugadores( id INT AUTO_INCREMENT PRIMARY KEY, nombre VARCHAR(100) NOT NULL);
+
+#Esto es el codigo para crear la base de datos del registro, NO BORRAR COMENTARIO. ATT: Barril de tuco#
+-->
